77e937d22f5d59713c653834c5a1bf33efebbca3
muneeb-ur-rehman <116435118+Muneeb-8055@users.noreply.github.com>
2025-02-13 13:21:36 +0500

    Update helpers.js/by muneeb

--- test/helpers.js
+++ test/helpers.js
@@ -5,3 +5,12 @@ var app = require('../server');
 global.app = app;
 global.expect = chai.expect;
 global.request = supertest(app);
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 3, sum;
+    sum = a + b;
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
+    return 0;
+}
